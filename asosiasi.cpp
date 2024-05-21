984e4af42bf371abcb433c5004afb8c6471c6413
prdpta <pradiptaputramei01@gmail.com>
2024-05-22 00:14:06 +0700

    menambahkan for

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -49,3 +49,8 @@ void dokter::tambahPasien(pasien* pPasien) {
 }
 void dokter::cetakPasien() {
     cout << "Daftar Pasien dari dokter \"" << this->nama << "\":\n";
+    for (auto& a : daftar_pasien) {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
